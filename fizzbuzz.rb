e3fef4f2702e1418831c28c36bc400bd721614d7
Rich Krakowski <richard.krakowski@wework.com>
2018-02-22 20:58:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,9 @@
-def fizzbuzz
-  
+def fizzbuzz(int)
+  if int % 3 == 0 
+    puts "Fizz"
+  elsif int % 5 == 0 
+    puts "Buss"
+  else int % 5 == 0 && int % 3 == 0 
+    puts "Fizzbuzz"
+  end
 end
